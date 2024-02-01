52918c0e474f68dedfc2f8022aa356f4448b43bd
carlosAlcia <carlosacia2@gmail.com>
2024-02-01 12:06:13 +0100

    [ADD] Train and predict functions from Pytorch example.

--- Main.py
+++ Main.py
@@ -2,10 +2,37 @@ import pathlib
 import tkinter as tk
 import tkinter.ttk as ttk
 import pygubu
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch
+import Image_Classifier as ImC
+
 
 PROJECT_PATH = pathlib.Path(__file__).parent
 PROJECT_UI = PROJECT_PATH / "Main_screen_UI.ui"
+PATH_NN = './cifar_net.pth'
 
+
+# Define a Convolutional Neural Network :
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 class Main_screen:
     def __init__(self, master=None):
@@ -27,8 +54,8 @@ class Main_screen:
 
     # Callback of Load Image button.
     def load_image(self):
-        print("hola\n")
-
+        print("hola, por ahora solo para hacer funcionar pytorch\n")
+        ImC.predict()
 
 if __name__ == '__main__':
     app = Main_screen()
